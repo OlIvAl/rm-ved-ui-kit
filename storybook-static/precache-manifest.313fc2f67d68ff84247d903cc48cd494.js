d8e85234dd53125d24c448fc0c34430ac1bb9992
gaisdav <ilike2dev@gmail.com>
2020-03-16 09:59:36 +0300

    сборка storybook-static

---
+++ storybook-static/precache-manifest.313fc2f67d68ff84247d903cc48cd494.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5331fbc102e91b86374eba3b52ee68e3",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cc2dd0d9c854d5ebb207.bundle.js"
+  },
+  {
+    "url": "runtime~main.cc2dd0d9c854d5ebb207.bundle.js"
+  },
+  {
+    "url": "vendors~main.cc2dd0d9c854d5ebb207.bundle.js"
+  },
+  {
+    "url": "vendors~main.cc2dd0d9c854d5ebb207.bundle.js.LICENSE.txt"
+  }
+]);
