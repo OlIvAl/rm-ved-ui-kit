bbccce99596412676a5887c53e569436bc096177
gaisdav <ilike2dev@gmail.com>
2020-03-23 17:02:57 +0300

    добавлены состояния для радионопок

--- src/components/RadioButton/RadioButton.stories.tsx
+++ src/components/RadioButton/RadioButton.stories.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Radio from "@material-ui/core/Radio";
+import { FormControl } from "@material-ui/core";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default {
   title: "RadioButtons",
@@ -7,11 +11,38 @@ export default {
 };
 
 export const RadioButtons = () => {
+  const [value, setValue] = React.useState("female");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+  };
   return (
-    <>
-      <Radio color="primary" />
-      <Radio color="secondary" />
-      <Radio color="default" />
-    </>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Gender</FormLabel>
+      <RadioGroup
+        aria-label="gender"
+        name="gender1"
+        value={value}
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="female"
+          control={<Radio color="primary" />}
+          label="Female"
+        />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel
+          value="other"
+          control={<Radio color="default" />}
+          label="Other"
+        />
+        <FormControlLabel
+          value="disabled"
+          disabled
+          control={<Radio />}
+          label="(Disabled option)"
+        />
+      </RadioGroup>
+    </FormControl>
   );
 };
