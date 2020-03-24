158b6869fd219ed2437054d9ec979d6571106dc4
gaisdav <ilike2dev@gmail.com>
2020-03-24 17:13:04 +0300

    добавлены состояния текста

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -17,14 +17,25 @@ export const Typographys = () => {
       <Typography variant="h6">h6</Typography>
       <Typography variant="subtitle1">subtitle1</Typography>
       <Typography variant="subtitle2">subtitle2</Typography>
-      <Typography>body1</Typography>
-      <Typography variant="body2">body2</Typography>
+      <Typography>body1 (default)</Typography>
       <Typography variant="caption" display="block">
         caption text
       </Typography>
-      <Typography variant="button" display="block">
+      <Typography gutterBottom variant="button" display="block">
         button text
       </Typography>
+      <div>
+        <Typography gutterBottom variant="subtitle1">
+          Состояния текста:
+        </Typography>
+        <Typography>body1 (initial) (default)</Typography>
+        <Typography color="primary">body1 (primary)</Typography>
+        <Typography color="secondary">body1 (secondary)</Typography>
+        <Typography color="textPrimary">body1 (textPrimary)</Typography>
+        <Typography color="textSecondary">body1 (textSecondary)</Typography>
+        <Typography color="error">body1 (error)</Typography>
+        <Typography color="inherit">body1 (inherit)</Typography>
+      </div>
     </>
   );
 };
