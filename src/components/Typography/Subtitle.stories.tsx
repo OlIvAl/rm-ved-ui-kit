e4a099fa669c4c98bbc2e650b53233115b2e1c95
gaisdav <ilike2dev@gmail.com>
2020-03-25 11:44:45 +0300

    компоненты Title и Subtitle

---
+++ src/components/Typography/Subtitle.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default {
+  title: "Typography",
+  component: Typography
+};
+
+export const Subtitle = () => {
+  return <Typography variant="subtitle1">Подзаголовки</Typography>;
+};
---
+++ src/components/Typography/Title.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default {
+  title: "Typography",
+  component: Typography
+};
+
+export const Title = () => {
+  return <Typography variant="h5">Заголовок страницы</Typography>;
+};
